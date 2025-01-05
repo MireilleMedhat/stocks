d7a294a7ebc41d932fab80ccfaecce171ee0e215
mireille <mireille.medhat@visionhealth.co.uk>
2025-01-05 19:52:02 +0200

    feat: add debounce logic using lodash

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,26 +1,37 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { API_KEY, BASE_URL } from '../constants';
+import { useEffect, useState, useMemo } from "react";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "../constants";
+import { debounce } from "lodash";
 
 const useSearch = (searchText: string): any => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
 
+  const fetchData = useMemo(
+    () =>
+      debounce(async (searchText: string) => {
+        setLoading(true);
+        try {
+          const response = await axios.get(
+            `${BASE_URL}&search=${searchText}&apiKey=${API_KEY}`
+          );
+          setData(response?.data);
+        } catch (err: any) {
+          setError(err);
+        } finally {
+          setLoading(false);
+        }
+      }, 300),
+    []
+  );
+
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`${BASE_URL}&search=${searchText}&apiKey=${API_KEY}`)
-      .then(function (response) {
-        setData(response?.data);
-      })
-      .catch(function (error) {
-        setError(error);
-      })
-      .finally(function () {
-        setLoading(false);
-      });
-  }, [searchText]);
+    fetchData(searchText);
+    return () => {
+      fetchData.cancel();
+    };
+  }, [searchText, fetchData]);
 
   return { data, loading, error };
 };
