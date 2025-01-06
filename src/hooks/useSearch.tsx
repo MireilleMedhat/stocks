cfdd4c836da48bb95d0a2025803ef5448113feec
mireille <mireille.medhat@visionhealth.co.uk>
2025-01-06 03:07:03 +0200

    feat: add logic for infinite scroll using next_url

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,39 +1,41 @@
-import { useEffect, useState, useMemo } from "react";
-import axios from "axios";
-import { API_KEY, BASE_URL } from "../constants";
-import { debounce } from "lodash";
+import { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
+import debounce from 'lodash/debounce';
+import { API_KEY, BASE_URL } from '../constants';
 
-const useSearch = (searchText: string): any => {
-  const [data, setData] = useState();
+const useSearch = (searchText: string) => {
+  const [data, setData] = useState<any[]>([]);
+  const [nextUrl, setNextUrl] = useState<string | null>(`${BASE_URL}&apiKey=${API_KEY}`);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const [error, setError] = useState<string | null>(null);
 
-  const fetchData = useMemo(
-    () =>
-      debounce(async (searchText: string) => {
-        setLoading(true);
-        try {
-          const response = await axios.get(
-            `${BASE_URL}&search=${searchText}&apiKey=${API_KEY}`
-          );
-          setData(response?.data);
-        } catch (err: any) {
-          setError(err);
-        } finally {
-          setLoading(false);
-        }
-      }, 300),
+  const fetchData = useCallback(
+    debounce(async (url: string) => {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        setData((prevData) => [...prevData, ...response?.data?.results]);
+        const next = response.data.next_url ?
+          `${response.data.next_url}&apiKey=${API_KEY}` :
+          `${BASE_URL}&apiKey=${API_KEY}`;
+        setNextUrl(next);
+      } catch (err: any) {
+        setError(err.message || 'An error occurred');
+      } finally {
+        setLoading(false);
+      }
+    }, 300),
     []
   );
 
   useEffect(() => {
-    fetchData(searchText);
+    fetchData(`${BASE_URL}&search=${searchText}&apiKey=${API_KEY}`);
     return () => {
       fetchData.cancel();
     };
   }, [searchText, fetchData]);
 
-  return { data, loading, error };
+  return { data, loading, error, loadMore: () => fetchData(nextUrl || '') };
 };
 
 export default useSearch;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,45 +1,44 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 import SearchBar from '../components/SearchBar/SearchBar';
 import useSearch from '../hooks/useSearch';
 import Footer from '../components/Footer/Footer';
 import Message from '../components/Message/Message';
-import Card from '../components/Card/Card';
 import StocksContainer from '../components/StocksContainer/StocksContainer';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const HomePage = () => {
   const [searchText, setSearchText] = useState('');
-  const [stocks, setStocks] = useState([]);
-
-  const { data, loading, error } = useSearch(searchText);
+  const { data: stocks, loading, error, loadMore } = useSearch(searchText);
 
   useEffect(() => {
     if (error) alert(error);
   }, [error]);
 
-  useEffect(() => {
-    if (data && data?.results) setStocks(data?.results);
-  }, [data]);
-
   return (
     <div className="container" data-testid="home-page">
       <div className="header-container">
         <h1 className="header">NASDAQ Stocks</h1>
-        <SearchBar
-          setSearchText={setSearchText}
-        />
+        <SearchBar setSearchText={setSearchText} />
       </div>
-      <div className="tableContainer">
-        {loading ? (
-          <Message messageText="Loading...please wait." />
-        ) : (
-          <StocksContainer stocks={stocks} />
-        )}
+
+      <div className="stocks-container" id="scrollable_div">
+        <InfiniteScroll
+          dataLength={stocks.length}
+          next={loadMore}
+          hasMore={!!stocks.length}
+          loader={<Message messageText="Loading more..." />}
+          scrollableTarget="scrollable_div"
+        >
+          {loading && stocks.length === 0 ? (
+            <Message messageText="Loading...please wait." />
+          ) : (
+            <StocksContainer stocks={stocks} />
+          )}
+        </InfiniteScroll>
       </div>
-      <Footer
-        resultsCount={data?.count}
-      />
+
+      <Footer resultsCount={stocks.length} />
     </div>
   );
 };
